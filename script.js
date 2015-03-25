dba4689b850c1704f5597c5cc3cb430bc2b9a875
Konrad Maurer <kradjr@gmail.com>
2015-03-25 17:48:05 -0600

    parsing and averaging functionality added.

--- script.js
+++ script.js
@@ -37,22 +37,87 @@ app.controller('ReviewCtrl', function($scope, $http) {
         noRatingCounter += 1;
       }
     }
-    
     return scoreSum / ($scope.data.totalReviews - noRatingCounter);
   }
 
-  $scope.addMonths = function (date, months) {
+  $scope.averageRatings = function (datum) {
+    var sum = 0;
+    var nullCounter = 0;
+    for (var i = 0; i < datum.length; i++) {
+      if (datum[i].rating != null) {
+        sum += datum[i].rating ;
+      } else {
+        nullCounter += 1;
+      }
+    }
+    return sum / (datum.length - nullCounter);
+  }
+
+  var addMonths = function (months) {
+    var date = new Date();
     date.setMonth(date.getMonth() + months);
+    // var newDate = date;
+    // newDate.setMonth(newDate.getMonth() + months);
     return date;
   }
 
-  $scope.pastSixMonthRatings = function () {
+  var pastSixMonthRatings = function () {
     var today = new Date();
-    $scope.sixMonthsAgo = $scope.addMonths(today, -6);
+    $scope.sixMonthsAgo = addMonths(today, -6);
+
   }();
+
+  var parseReviewsByMonth = function () {
+    var today = new Date();
+    var sixAgo = addMonths(-6);
+    var fiveAgo = addMonths(-5);
+    var fourAgo = addMonths(-4);
+    var threeAgo = addMonths(-3);
+    var twoAgo = addMonths(-2);
+    var oneAgo = addMonths(-1);
+
+    $scope.sixAgoObjects = [];
+    $scope.fiveAgoObjects = [];
+    $scope.fourAgoObjects = [];
+    $scope.threeAgoObjects = [];
+    $scope.twoAgoObjects = [];
+    $scope.oneAgoObjects = [];
+
+    for (var review in $scope.data.reviews) {
+      var revObj = $scope.data.reviews[review];
+      var revDate = new Date(revObj.publishDate);
+      console.log(typeof(revDate));
+      if ( revDate >= sixAgo && revDate < fiveAgo) {
+        $scope.sixAgoObjects.push(revObj);
+      } else if (revDate >= fiveAgo && revDate < fourAgo){
+        $scope.fiveAgoObjects.push(revObj);
+      } else if (revDate >= fourAgo && revDate < threeAgo){
+        $scope.fourAgoObjects.push(revObj);
+      } else if (revDate >= threeAgo && revDate < twoAgo){
+        $scope.threeAgoObjects.push(revObj);
+      } else if (revDate >= twoAgo && revDate < oneAgo){
+        $scope.twoAgoObjects.push(revObj);
+      } else if (revDate >= oneAgo && revDate < today){
+        $scope.oneAgoObjects.push(revObj);
+      }
+    }
+      
+      // console.log($scope.sixAgoObjects);
+      // console.log($scope.fiveAgoObjects);
+      // console.log($scope.fourAgoObjects);
+      // console.log($scope.threeAgoObjects);
+      // console.log($scope.twoAgoObjects);
+      // console.log($scope.oneAgoObjects);
+  }();
+
+  console.log($scope.averageRatings($scope.sixAgoObjects));
 
 })
 
+
+
+
+// setup for ui-router
 app.config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /state1
