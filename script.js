216a154e31387cb8e57f9cc0cb5775af5bb7046b
Konrad Maurer <kradjr@gmail.com>
2015-03-25 14:17:45 -0600

    basic ui-router functionality in place. and calculating six months ago function.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 
-var app = angular.module('reviewData', []);
+var app = angular.module('reviewData', ['ui.router']);
 
 app.controller('ReviewCtrl', function($scope, $http) {
 
-  // initial http request gets page 1 data and totalPages to loop through all pages.
+  // initial http request gets page 1 data and totalPages to loop through all pages, sorted by publishDate.
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://secure.reviewtrackers.com/api/locations/18598/reviews?api_token=274b859d23b5e2778b46&sort=publishDate&order=desc&per_page=50&page=1", false);
   xhr.send();
@@ -37,9 +37,49 @@ app.controller('ReviewCtrl', function($scope, $http) {
         noRatingCounter += 1;
       }
     }
+    
     return scoreSum / ($scope.data.totalReviews - noRatingCounter);
   }
 
+  $scope.addMonths = function (date, months) {
+    date.setMonth(date.getMonth() + months);
+    return date;
+  }
+
+  $scope.pastSixMonthRatings = function () {
+    var today = new Date();
+    $scope.sixMonthsAgo = $scope.addMonths(today, -6);
+  }();
 
 })
 
+app.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/bysite");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('bysite', {
+      url: "/bysite",
+      templateUrl: "partials/bysite.html"
+    })
+    .state('bysite.list', {
+      url: "/list",
+      templateUrl: "partials/bysite.list.html",
+      controller: function($scope) {
+        $scope.items = ["A", "List", "Of", "Items"];
+      }
+    })
+    .state('byrating', {
+      url: "/byrating",
+      templateUrl: "partials/byrating.html"
+    })
+    .state('byrating.list', {
+      url: "/list",
+      templateUrl: "partials/byrating.list.html",
+      controller: function($scope) {
+        $scope.things = ["A", "Set", "Of", "Things"];
+      }
+    });
+});
