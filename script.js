5c677c99560238af5e2b328d9bdcb5108fd0ee4c
Konrad Maurer <kradjr@gmail.com>
2015-03-25 08:50:24 -0600

    refactor and clean initial http requests.

--- script.js
+++ script.js
@@ -2,68 +2,28 @@
 
 var app = angular.module('reviewData', []);
 
-
-
-
 app.controller('ReviewCtrl', function($scope, $http) {
 
-  // $http({method: 'GET', url: 'https://secure.reviewtrackers.com/api/locations/18598/reviews?api_token=274b859d23b5e2778b46&sort=publishDate&order=desc&per_page=50&page=1'}).success(function(data) {
-  //   $scope.data = data; // response data 
-  // });
-
-  // var more_pages = true;
-  // do 
-
-  // while (more_pages == true);
-
+  // initial http request gets page 1 data and totalPages to loop through all pages.
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://secure.reviewtrackers.com/api/locations/18598/reviews?api_token=274b859d23b5e2778b46&sort=publishDate&order=desc&per_page=50&page=1", false);
   xhr.send();
-
   console.log(xhr.status);
   console.log(xhr.statusText);
-
   $scope.data = angular.fromJson(xhr.response);
 
-  var reviews = $scope.data.reviews;
-  console.log(reviews);
-
+  // loop through remaining pages to get all reviews
   for (var i = 2; i <= $scope.data.totalPages; i++) {
     var page = new XMLHttpRequest();
     page.open("GET", "https://secure.reviewtrackers.com/api/locations/18598/reviews?api_token=274b859d23b5e2778b46&sort=publishDate&order=desc&per_page=50&page=" + i.toString(), false);
     page.send();
-
-
     var jsonPage = angular.fromJson(page.response);
-
     for (var review in jsonPage.reviews) {
        var obj = jsonPage.reviews[review];
-       reviews.push(obj);
+       $scope.data.reviews.push(obj);
     }
   }
 
-  $scope.reviews = reviews;
-
-  //console.log($scope.reviews);
+  console.log($scope.data);
 })
 
-// app.controller('ReviewCtrl', function($scope, $http) {
-//     $http.get("https://secure.reviewtrackers.com/api/locations/18598/reviews?api_token=274b859d23b5e2778b46&sort=publishDate&order=desc&per_page=50&page=1")
-//     .success(function(response) {$scope.data = response}).
-//     error( function(response) {$scope.data = response});
-
-//     console.log($scope.data);
-
-// });
-
-
-// // Simple GET request example :
-// $http.get('https://secure.reviewtrackers.com/api/locations/18598/reviews?api_token=274b859d23b5e2778b46&sort=publishDate&order=desc&per_page=50&page=1').
-//   success(function(data, status, headers, config) {
-//     // this callback will be called asynchronously
-//     // when the response is available
-//   }).
-//   error(function(data, status, headers, config) {
-//     // called asynchronously if an error occurs
-//     // or server returns response with an error status.
-//   });
